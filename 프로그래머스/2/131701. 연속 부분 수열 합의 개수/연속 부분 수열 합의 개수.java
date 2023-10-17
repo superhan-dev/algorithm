325fae95ed7f4af247c4576f2074e6440a4a7bb2
superhan-dev <superhan.dev@gmail.com>
2023-10-17 23:51:24 +0900

    알고리즘 제출 후 메모 추가

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -2,27 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] elements) {
-        int answer = 0;
-        
         int[] newElements = new int[elements.length * 2];
-        
-        for(int i=0;i<elements.length;i++){
-            newElements[i] = newElements[i+elements.length] = elements[i];
+
+        for (int i = 0; i < elements.length; i++) {
+            newElements[i] = newElements[i + elements.length] = elements[i];
         }
-        
+
         Set<Integer> set = new HashSet<>();
 
-        for(int i=0;i<elements.length;i++){
-            
-            if(i == 0){
+        for (int i = 0; i < elements.length; i++) {
+
+            if (i == 0) {
                 set.add(Arrays.stream(elements).sum());
             } else {
-                 for(int j=0;j<elements.length;j++){
-                    set.add(Arrays.stream(newElements,j,j+i).sum());
-                }    
+                for (int j = 0; j < elements.length; j++) {
+                    set.add(Arrays.stream(newElements, j, j + i).sum());
+                }
             }
         }
-        
+
         return set.size();
     }
 }
