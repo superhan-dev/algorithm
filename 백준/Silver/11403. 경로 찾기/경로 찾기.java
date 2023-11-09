999b11b3bfbe5d6e3a9e970302ac7960c5098695
SangHyup Han <88954587+superhan-dev@users.noreply.github.com>
2023-11-09 20:16:59 +0900

    [Silver I] Title: 경로 찾기, Time: 208 ms, Memory: 17044 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[][] arr = new int[n][n];
+    for (int i = 0; i < n; i++) {
+      String[] temp = br.readLine().split(" ");
+      for (int j = 0; j < temp.length; j++) {
+        arr[i][j] = Integer.parseInt(temp[j]);
+      }
+    }
+
+    arr = floydWarshall(arr, n);
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[i].length; j++) {
+        sb.append(arr[i][j] + " ");
+      }
+      sb.append("\n");
+    }
+    System.out.println(sb);
+
+  }
+
+  static int[][] floydWarshall(int[][] dist, int n) {
+
+    for (int k = 0; k < n; k++) {
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          if (dist[i][k] == 1 && dist[k][j] == 1) {
+            dist[i][j] = 1;
+          }
+        }
+      }
+    }
+
+    return dist;
+  }
+}
