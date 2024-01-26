f038572ad28b97713e8cd786c009810284b8fe6a
SangHyup Han <88954587+superhan-dev@users.noreply.github.com>
2024-01-27 09:17:51 +1100

    [level 0] Title: 수 조작하기 1, Time: 1.08 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = n;
+        
+        for(char c : control.toCharArray()){
+            if(c == 'w'){
+                answer += 1;
+            } else if(c == 's'){
+                answer -= 1;
+            } else if(c == 'd'){
+                answer += 10;
+            } else if(c == 'a'){
+                answer -= 10;
+            }      
+        }
+        
+        
+        return answer;
+    }
+}
