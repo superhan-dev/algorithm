22ff397a4faa13025e2cc5023e01977656d2f4b5
SangHyup Han <88954587+superhan-dev@users.noreply.github.com>
2025-05-25 09:31:46 +0900

    [level 2] Title: 오픈채팅방, Time: 194.72 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -2,32 +2,28 @@
 
 function solution(record) {
     const user = {}   
-    
-    for(const r of record) {
-        const curr = r.split(' ');
-        
-        const id = curr[1];
-        const name = curr[2];
-        user[id] = {
-          name: name ? name : user[id].name,
-        }  
+    const action = [];
+    const stateMap = {
+        'Enter': '들어왔습니다.',
+        'Leave': '나갔습니다.',
     }
     
-    const answer = [];
-    for(const r of record) {
-        const curr = r.split(' ');
+    record.forEach((r) => {
+        const [state,id,name] = r.split(' ');
         
-        const cmd = curr[0];
-        const id = curr[1];
-        const name = curr[2];
-        
-        if(cmd === 'Enter') {
-            answer.push(`${user[id].name}님이 들어왔습니다.`)
+        if(state !== 'Change') {
+            action.push([state,id]);
         } 
-        if(cmd === 'Leave') {
-            answer.push(`${user[id].name}님이 나갔습니다.`);
+        
+        if(name) {
+            user[id] = {
+                name:name
+            };
         }
-    }
+    });
     
-    return answer;
+    return action.map(([state,id]) => {
+        return `${user[id].name}님이 ${stateMap[state]}`;
+    })
+    
 }
