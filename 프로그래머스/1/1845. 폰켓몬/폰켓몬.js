cbca53e9046bc7620de85ce61b82b8f3884a00a9
SangHyup Han <88954587+superhan-dev@users.noreply.github.com>
2025-06-01 14:15:56 +0900

    [level 1] Title: 폰켓몬, Time: 0.57 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,14 +1,6 @@
 function solution(nums) {
-    let answer = -1;
-    const set = new Set();
-    nums.forEach((num) => {
-        set.add(num);
-        
-        if(set.size <= nums.length / 2){
-            answer = set.size;
-            return;
-        }
-    })
+    const max = nums.length / 2;
+    const set = new Set([...nums]);
     
-    return answer;
+    return set.size > max ? max : set.size;
 }
